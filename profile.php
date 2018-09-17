0950ca6ba4722fd5bf1207e95335562f6c1f7132
Nopphapadon <43329424+Nopphapadon@users.noreply.github.com>
2018-09-17 15:44:28 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ByA6ZDJdAHqEOUrtzDU23arxqQ3VQeAuJQ8UpytrDdcTKfTrXsM74ntHVbTWNCAyn5iUaU8KGSie5q7B5NY1xDdTS+RmTHf7vV5L0p0mUHhm15+Yz8GHxao5DD66JxWTBtqGd3vjLyRNfFYNGEPn0gdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U8159d10249e16b6ed051ab6fae5d3e2d';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
